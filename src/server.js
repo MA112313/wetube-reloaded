58c47f71823ccae8941f7b87e7f622309ede48b8
MA112313 <113953778+MA112313@users.noreply.github.com>
2022-11-11 18:39:57 +0900

    유튜브 클론코딩
    ?~#6.4

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
+const PORT = 4000;
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(express.urlencoded({ extended: true }));
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
